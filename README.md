601e45883595e8add5f4df4da3b75d7289497abf
sehun98 <sehun4270011@gmail.com>
2026-04-06 21:12:05 +0900

    tick generator 1ms accumulator

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-\[2026-04-06] verilogHDL 환경 설정 및 기초 문법
+\[2026-04-06] 
+1. verilogHDL 환경 설정 및 기초 문법
 
-[2026-04-06 방과후] tick generator 1ms
+[2026-04-06 방과후] 
+1. tick generator 1ms
+2. tick generator 1ms accumulator
--- software/20260406_watch/20260406_watch.srcs/sources_1/new/tick_generator_1ms.v
+++ software/20260406_watch/20260406_watch.srcs/sources_1/new/tick_generator_1ms.v
@@ -1,35 +1,32 @@
 `timescale 1ns / 1ps
 
-module tick_generator_1ms (
+// accumulator
+module tick_generator_1ms(
     input  wire clk,
     input  wire rst_n,
     output reg  tick_1ms
 );
 
-    // ===== Parameters =====
-    parameter CLK_FREQ_HZ = 100_000_000;  // 입력 클럭 (Hz)
-    parameter TICK_RATE_HZ = 1000;  // 1ms = 1kHz
+parameter CLK_FREQ_HZ  = 100_000_000;
+parameter TICK_RATE_HZ = 1000;
 
-    localparam CLK_DIV = CLK_FREQ_HZ / TICK_RATE_HZ;
-    localparam COUNTER_WIDTH = $clog2(CLK_DIV);
+localparam ACC_STEP    = TICK_RATE_HZ;
+localparam ACC_WIDTH   = $clog2(CLK_FREQ_HZ);
+reg [ACC_WIDTH:0] phase_acc; // ACC_WIDTH-1 -> ACC_WIDTH
 
-    // ===== Registers =====
-    reg [COUNTER_WIDTH-1:0] cycle_cnt;
-
-    // ===== Logic =====
-    always @(posedge clk or negedge rst_n) begin
-        if (!rst_n) begin
-            cycle_cnt <= 0;
-            tick_1ms  <= 0;
+always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+        phase_acc <= 0;
+        tick_1ms  <= 0;
+    end else begin
+        if (phase_acc + ACC_STEP >= CLK_FREQ_HZ) begin
+            phase_acc <= phase_acc + ACC_STEP - CLK_FREQ_HZ;
+            tick_1ms  <= 1;
         end else begin
-            if (cycle_cnt == CLK_DIV - 1) begin
-                cycle_cnt <= 0;
-                tick_1ms  <= 1;
-            end else begin
-                cycle_cnt <= cycle_cnt + 1;
-                tick_1ms  <= 0;
-            end
+            phase_acc <= phase_acc + ACC_STEP;
+            tick_1ms  <= 0;
         end
     end
+end
 
 endmodule
